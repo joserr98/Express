5a0901d7869c17e001663c0dcdef167ad4b9ff87
joserr98 <joserr9807@gmail.com>
2023-04-24 13:16:22 +0200

    Railway 3.0

--- conf.ts
+++ conf.ts
@@ -2,7 +2,7 @@ const config = {
     SECRET: process.env.SECRET!,
     DB: process.env.DB!,
     SALT_ROUND: Number(process.env.SALT_ROUND)!,
-    PORT: Number(process.env.PORT)!
+    PORT: process.env.PORT!
 }
 
 export default config
